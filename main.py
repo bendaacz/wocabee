6149f070f6c9c28028c7704ef65319bac8b2cb2b
ben <benedikt.student@gmail.com>
2025-12-07 18:29:20 +0100

    feat: load exercise using balicekID

--- main.py
+++ main.py
@@ -24,3 +24,19 @@ password.send_keys(os.getenv("PASSWORD"))
 send.click()
 
 WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "flag"))).click()
+
+url = driver.current_url
+trida = url[url.find("class_id=")+9:url.find("&refresh=")]
+
+print("cislo balicku (napr. 1234567): ")
+balicekID = input()
+
+print("rezim cviceni: ")
+mode = input()
+
+print(
+    "\nbalicek: " + balicekID,
+    "\ntrida: " + trida, 
+    "\nrezim: " + mode)
+    
+driver.find_element(By.ID, value=f"btnRun{balicekID}").click()
